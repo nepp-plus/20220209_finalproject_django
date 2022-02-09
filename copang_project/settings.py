28892c47139de8faa099b38ecd6450cacf6b0221
nepp.plus <nepp.plus@gmail.com>
2022-02-09 10:00:13 +0900

    rest_framework 설치 / 세팅

--- copang_project/settings.py
+++ copang_project/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'copang_app'
+    'copang_app',
+    'rest_framework',
 ]
 
 APPEND_SLASH = False
