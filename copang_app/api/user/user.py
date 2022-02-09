9f820e4139a6b8830c06e314a78e861df8495f05
nepp.plus <nepp.plus@gmail.com>
2022-02-09 11:22:57 +0900

    Users 모델에 비번 암호화 관련 기능 추가
     - 로그인시에 활용

--- copang_app/api/user/user.py
+++ copang_app/api/user/user.py
@@ -22,15 +22,21 @@ class User(APIView):
             # 임시 - 비번은 암호화 되어있고, django에서는 아직 기능 구현 안됨.
             # 이메일 만 맞으면 성공.
             
-            user_serialized = UsersSerializer(email_user)
-            
-            return Response({
-                'code': 200,
-                'message': '임시- 로그인 성공',
-                'data': {
-                    'user': user_serialized.data,
-                }
-            })
+            if email_user.is_same_password(input_pw):
+                user_serialized = UsersSerializer(email_user)
+                
+                return Response({
+                    'code': 200,
+                    'message': '로그인 성공',
+                    'data': {
+                        'user': user_serialized.data,
+                    }
+                })
+            else:
+                return Response({
+                    'code': 400,
+                    'message': '비밀번호가 틀립니다.'
+                }, status=400)
         else:
             return Response({
                 'code': 400,
